54c7cfff1c336a6aa4e44e7dc531c3d0db5a8d95
Jess Smith <smith.jessk@gmail.com>
2015-09-12 01:40:57 -0400

    PA1: Clean up pa1.cpp

--- pa1/src/pa1.cpp
+++ pa1/src/pa1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
+#include <string>
 
 int promptForSize() {
   std::cout << "Enter the size of a magic square: ";
   int size;
   std::cin >> size;
-  std::cout << std::endl;
   return size;
 }
 
 bool checkSize(int size) {
   bool isOdd = (size % 2 == 1);
-  bool isInRange = (size >= 3 && size <= 15);
+  bool isInRange = (size >= 3) && (size <= 15);
   return isOdd && isInRange;
 }
 
@@ -18,8 +18,9 @@ int main() {
   int size = promptForSize();
   bool validSize = checkSize(size);
   if (!validSize) {
-    std::cout << "You entered an invalid magic square size." << std::endl <<
-      "Size must be an odd integer between 3 and 15, inclusive." << std::endl;
+    std::string message = "You entered an invalid magic square size.\n" 
+      "Size must be an odd integer between 3 and 15, inclusive.\n";
+    std::cout << message;
     return 1;
   } else {
     // MagicSquare square;
