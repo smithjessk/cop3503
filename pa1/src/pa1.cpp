f2f380d9bd4503cdf167e64202efee3b44ba1397
Jess Smith <smith.jessk@gmail.com>
2015-09-23 23:33:28 -0400

    PA1: Add documentation and check that magic square built correctly

--- pa1/src/pa1.cpp
+++ pa1/src/pa1.cpp
@@ -5,11 +5,12 @@
 #include "pa1.h"
 
 /**
+ * Find the positive integer n such that n = number (mod) modulo.
  * Similar functionality to number % modulo, except this always ensures a 
  * positive result.
  * @param  number The number to take the mod of
  * @param  modulo The number by which we divide
- * @return        The positive integer n such that n = number (mod) modulo.
+ * @return        The number n as described above.
  */
 int takePositiveMod(int number, int modulo) {
   int result = number % modulo;
@@ -20,16 +21,17 @@ int takePositiveMod(int number, int modulo) {
 }
 
 MagicSquare::MagicSquare(int inputtedLength) {
-  this->length = inputtedLength;
+  length = inputtedLength;
   if (isValidSize()) {
-    grid.resize(length);
-    for (int i = 0; i < length; i++) {
-      grid[i].resize(length);
-    }
+    resizeGrid();  
   }
 }
 
 void MagicSquare::resizeGrid() {
+  grid.resize(length);
+  for (int i = 0; i < length; i++) {
+    grid[i].resize(length);
+  }
   for (int i = 0; i < length; i++) {
     for (int j = 0; j < length; j++) {
       grid[i][j] = -1;
@@ -70,6 +72,11 @@ int MagicSquare::rightDiagSum() {
   return sum;
 }
 
+/**
+ * Determine if the length field on this MagicSquare instance is valid. Namely,
+ * assert that it must be odd and between the numbers 3 and 15, inclusive.
+ * @return Whether or not size fits the above descriptions.
+ */
 bool MagicSquare::isValidSize() {
   bool isOdd = (length % 2 == 1);
   bool isInRange = (length >= 3) && (length <= 15);
@@ -77,12 +84,10 @@ bool MagicSquare::isValidSize() {
 }
 
 /**
- * The magic constant is the value to which every row, column, and diagonal
- * sums to.
+ * 
  * Source: http://mathworld.wolfram.com/MagicSquare.html
  */
 void MagicSquare::build() {
-  resizeGrid();
   int row = 0;
   int col = length / 2;
   int numSet = 0;
@@ -104,19 +109,41 @@ void MagicSquare::build() {
   }
 }
 
-void MagicSquare::test() {
+/**
+ * Check and print the sum of each row, column, and major diagonal in the grid.
+ * @return Whether or not the each value currentSum took on was the same as the
+ * value of acceptedSum.
+ */
+bool MagicSquare::test() {
+  int acceptedSum = rowSum(0);
+  int currentSum;
+  bool consistentSums = true;
   std::cout << "Checking the sums of every row: ";
   for (int i = 0; i < length; i++) {
-    std::cout << rowSum(i) << " ";
+    currentSum = rowSum(i);
+    std::cout << currentSum << " ";
+    if (currentSum != acceptedSum) {
+      consistentSums = false;
+    }
   }
   std::cout << "\nChecking the sums of every column: ";
   for (int i = 0; i < length; i++) {
-    std::cout << colSum(i) << " ";
+    currentSum = colSum(i);
+    std::cout << currentSum << " ";
+    if (currentSum != acceptedSum) {
+      consistentSums = false;
+    }
   }
   std::cout << "\nChecking the sums of every diagonal: ";
   std::cout << leftDiagSum() << " " << rightDiagSum() << std::endl;
+  if (leftDiagSum() != acceptedSum || rightDiagSum() != acceptedSum) {
+    consistentSums = false;
+  }
+  return consistentSums;
 }
 
+// Prompt for and return what the user wants the side length of the magic
+// square to be. 
 int promptForSideLength() {
   std::cout << "Enter the side length of a magic square: ";
   int length;
@@ -124,6 +151,13 @@ int promptForSideLength() {
   return length;
 }
 
+/**
+ * Take in a side length. If it is valid, build and test a magic square with
+ * that side length. If the magic square does not pass the test() method, then 
+ * return 1.
+ * If the size was invalid, print out an appropriate error message and return 1
+ * to denote that something went wrong.
+ */
 int main() {
   int length = promptForSideLength();
   MagicSquare square(length);
@@ -134,7 +168,13 @@ int main() {
     return 1;
   } else {
     square.build();
-    square.test();
-    return 0;
+    bool buildPassed = square.test();
+    if (!buildPassed) {
+      std::cout << "The square could not be correctly built." << std::endl;
+      return 1;
+    } else {
+      std::cout << "Build successful." << std::endl;
+      return 0;
+    }
   }
 }
--- pa1/src/pa1.h
+++ pa1/src/pa1.h
@@ -19,44 +19,61 @@ class MagicSquare {
   std::vector<std::vector<int> > grid;
 
   // Resize grid to length * length, setting each entry to -1. 
-  // Used in the build() method. 
+  // Used in the constructor if the inputted length was valid.
   void resizeGrid();
 
-  // Calculate the sum of the elements of the passed row. Note that the row
-  // indices start at 0 and continue in the same manner as normal vector 
-  // indices.
+  /** 
+   * Calculate the sum of the elements of the passed row. Note that the row
+   * indices start at 0 and continue in the same manner as normal vector 
+   * indices.
+   */
   int rowSum(int row);
 
-  // Calculate the sum of the elements of the passed column. Note that the 
-  // column indices start at 0 and continue in the same manner as normal 
-  // vector indices.
+  /**
+   * Calculate the sum of the elements of the passed column. Note that the
+   * column indices start at 0 and continue in the same manner as normal 
+   * vector indices.
+   */
   int colSum(int col);
 
-  // Calculate the sum of the grid entries along the main "upper-left"
-  // diagonal. That is, the entries (0, 0), (2, 2), and so on until 
-  // (length - 1, length - 1).
+  /**
+   * Calculate the sum of the grid entries along the main "upper-left"
+   * diagonal. That is, the entries (0, 0), (2, 2), and so on until 
+   * (length - 1, length - 1).
+   */
   int leftDiagSum();
 
-  // Calculate the sum of the grid entries along the main "upper-right" 
-  // diagonal. That is, the entries (length - 1, 0), (length - 2, 1), and so 
-  // on until (0, length - 1). Note that (i, j) correspo nds to the i-th row
-  // and j-th column.
+  /**
+   * Calculate the sum of the grid entries along the main "upper-right"
+   * diagonal. That is, the entries (length - 1, 0), (length - 2, 1), and so 
+   * on until (0, length - 1). Note that (i, j) correspo nds to the i-th row
+   * and j-th column.
+   */
   int rightDiagSum();
 
  public:
+  /**
+   * Take an inputted length and, if appropriate, as determined by 
+   * isValidSize(), then call resizeGrid().
+   */
   MagicSquare(int length);
 
-  // Checks if the matrix is a valid size. Clled before the grid is actually
-  // created in order to prevent the allocation of, in the worst case, a 
-  // million by million matrix.
+  /**
+   * Check if the matrix is a valid size. Do so before the grid is actually
+   * created in order to prevent the allocation of, in the worst case, a 
+   * million by million matrix.
+   */
   bool isValidSize();
 
-  // Create the matrix. Namely, follow the algorithm outlined in 
-  // http://mathworld.wolfram.com/MagicSquare.html
+  /**
+   * Create the matrix. Namely, follow the algorithm outlined in 
+   * http://mathworld.wolfram.com/MagicSquare.html
+   */
   void build();
 
-  // Print out the sum of each row, column, and diagonal. 
-  void test();
+  // Print out the sum of each row, column, and diagonal. Return whether or 
+  // not all of the sums are the same. 
+  bool test();
 };
 
 #endif // PA1_HEADER
